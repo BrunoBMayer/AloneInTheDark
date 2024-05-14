ca6620e18bf3d6bfb7e566534c259f6ad6be54c4
Your Name <brunobrmayer@gmail.com>
2024-05-14 07:16:12 -0500

    Fim de jogo

--- src/main.c
+++ src/main.c
@@ -1,6 +1,5 @@
-// Código principal do jogo
-#include <string.h>
 #include <stdio.h>
+#include <unistd.h>
 #include "screen.h"
 #include "keyboard.h"
 #include "timer.h"
@@ -52,8 +51,22 @@ int main() {
             char key = readch();
 
             // Verifica se a tecla Enter foi pressionada para encerrar o programa
-            if (key == '\n') 
+            if (key == '\n') {
+                printf("\n.d888 d8b                          888                  d8b                                     88b.   \n");
+                printf("d88P\"  Y8P                         888                  Y8P                                     \"Y88b  \n");
+                printf("888                                888                                                            Y88b \n");
+                printf("888888 888 88888b.d88b.        .d88888  .d88b.         8888  .d88b.   .d88b.   .d88b.       d8b    888 \n");
+                printf("888    888 888 \"888 \"88b      d88\" 888 d8P  Y8b        \"888 d88\"\"88b d88P\"88b d88\"\"88b      Y8P    888 \n");
+                printf("888    888 888  888  888      888  888 88888888         888 888  888 888  888 888  888            d88P \n");
+                printf("888    888 888  888  888      Y88b 888 Y8b.             888 Y88..88P Y88b 888 Y88..88P      d8b .d88P  \n");
+                printf("888    888 888  888  888       \"Y88888  \"Y8888          888  \"Y88P\"   \"Y88888  \"Y88P\"       Y8P 88P\"   \n");
+                printf("                                                        888               888                          \n");
+                printf("                                                       d88P          Y8b d88P                          \n");
+                printf("                                                     888P\"            \"Y88P\"                          \n");
+
+                sleep(4); // Atraso de 4 segundos
                 break;
+            }
 
             // Move o ponto com base na tecla pressionada
             movePoint(&x, &y, key);
